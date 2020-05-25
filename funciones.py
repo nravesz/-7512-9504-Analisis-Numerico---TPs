a9a45ca91de4fefd4fddb168e50381b7d19aac27
Ramaa <ramaa196@gmail.com>
2020-05-25 17:01:02 -0300

    derivadas 2das

--- funciones.py
+++ funciones.py
@@ -1,23 +1,32 @@
 import numpy as np
-import matplotlib.plotly as plt
+import matplotlib.pyplot as plt
 
 def f1(x):
     return x**2 - 2
 
-def f1_derivada(x):
+def f1_derivada1(x):
     return 2*x
+
+def f1_derivada2(x):
+    return 2
 
 def f2(x):
     return x**5 - 6.6 * x**4 + 5.12 * x**3 + 21.312 * x**2 - 38.016 * x + 17.28
 
-def f2_derivada(x):
+def f2_derivada1(x):
     return 5 * x**4 -26.4 * x**3 + 15.36 * x**2 + 42.624 * x - 38.016
+
+def f2_derivada2(x):
+    return 20 * x**3 - 79.2 * x**2 + 30.72 * x + 42.624
 
 def f3(x):
     return (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2)
 
-def f3_derivada(x):
+def f3_derivada1(x):
     return (-8 * x + 12) * (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2) + np.exp(-4 * (x - 1.5)**2)
+
+def f3_derivada2(x):
+    return (-24 * x + (x - 1.5) * ((8 * x - 12.0)**2) + 36.0) * np.exp(-4 * (x - 1.5)**2)
 
 def imprimirFuncion(funcion):
     nroResolucion = 250
