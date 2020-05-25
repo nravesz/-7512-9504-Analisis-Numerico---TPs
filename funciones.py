1ee84453544a82dd2b1494cd08f2caaf86977bd7
xNicky <nraveszani@gmail.com>
2020-05-25 17:32:45 -0300

    Las funciones f1, f2 y f3 ahora son clases, asi no pasamos tantos argumentos a los metodos

--- funciones.py
+++ funciones.py
@@ -1,32 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from abc import ABCMeta, abstractmethod
 
-def f1(x):
-    return x**2 - 2
+class Funcion(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def evaluar_funcion(self, x):
+        pass
+    
+    @abstractmethod
+    def evaluar_derivada_primera(self, x):
+        pass
+    
+    @abstractmethod
+    def evaluar_derivada_segunda(self, x):
+        pass
 
-def f1_derivada1(x):
-    return 2*x
+class F1(Funcion):
+    
+    def evaluar_funcion(self, x):
+        return x**2 - 2
+    
+    def evaluar_derivada_primera(self, x):
+        return 2*x
+    
+    def evaluar_derivada_segunda(self, x):
+        return 2
 
-def f1_derivada2(x):
-    return 2
+class F2(Funcion):
+    
+    def evaluar_funcion(self, x):
+        return x**5 - 6.6 * x**4 + 5.12 * x**3 + 21.312 * x**2 - 38.016 * x + 17.28
+    
+    def evaluar_derivada_primera(self, x):
+        return 5 * x**4 -26.4 * x**3 + 15.36 * x**2 + 42.624 * x - 38.016
+    
+    def evaluar_derivada_segunda(self, x):
+        return 20 * x**3 - 79.2 * x**2 +30.72 * x + 42.624
 
-def f2(x):
-    return x**5 - 6.6 * x**4 + 5.12 * x**3 + 21.312 * x**2 - 38.016 * x + 17.28
-
-def f2_derivada1(x):
-    return 5 * x**4 -26.4 * x**3 + 15.36 * x**2 + 42.624 * x - 38.016
-
-def f2_derivada2(x):
-    return 20 * x**3 - 79.2 * x**2 + 30.72 * x + 42.624
-
-def f3(x):
-    return (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2)
-
-def f3_derivada1(x):
-    return (-8 * x + 12) * (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2) + np.exp(-4 * (x - 1.5)**2)
-
-def f3_derivada2(x):
-    return (-24 * x + (x - 1.5) * ((8 * x - 12.0)**2) + 36.0) * np.exp(-4 * (x - 1.5)**2)
+class F3(Funcion):
+    
+    def evaluar_funcion(self, x):
+        return (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2)
+    
+    def evaluar_derivada_primera(self, x):
+        return (-8 * x + 12) * (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2) + np.exp(-4 * (x - 1.5)**2)
+    
+    def evaluar_derivada_segunda(self, x):
+        return (-24 * x + (x - 1.5) * (8 *x - 12)**2 + 36) * np.exp(-4 * (x - 1.5)**2)
 
 def imprimirFuncion(funcion):
     nroResolucion = 250
