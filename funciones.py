ab5a37f44e70069a98b5c853b2857e53d4f603e2
juankristal <juankristal@hotmail.com>
2020-05-27 17:50:18 -0300

    Defino clase Funcion sin abstracción.

--- funciones.py
+++ funciones.py
@@ -2,66 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from abc import ABCMeta, abstractmethod
 
-class Funcion(metaclass=ABCMeta):
+class Funcion():
     
-    @abstractmethod
-    def evaluar_funcion(self, x):
-        pass
-    
-    @abstractmethod
-    def evaluar_derivada_primera(self, x):
-        pass
-    
-    @abstractmethod
-    def evaluar_derivada_segunda(self, x):
-        pass
-    
-    @abstractmethod
-    def imprimir_nombre(self):
-        pass
+	def __init__(self, denominacion, expresion, derivada_primera, derivada_segunda):
+		self.denominacion = denominacion
+		self.expresion = expresion
+		self.derivada_primera = derivada_primera
+		self.derivada_segunda = derivada_segunda
 
-class F1(Funcion):
-    
     def evaluar_funcion(self, x):
-        return x**2 - 2
-    
-    def evaluar_derivada_primera(self, x):
-        return 2*x
-    
-    def evaluar_derivada_segunda(self, x):
-        return 2
-    
-    def imprimir_nombre(self):
-        return "f1(x)"
-    
-class F2(Funcion):
-    
-    def evaluar_funcion(self, x):
-        return x**5 - 6.6 * x**4 + 5.12 * x**3 + 21.312 * x**2 - 38.016 * x + 17.28
-    
-    def evaluar_derivada_primera(self, x):
-        return 5 * x**4 -26.4 * x**3 + 15.36 * x**2 + 42.624 * x - 38.016
-    
-    def evaluar_derivada_segunda(self, x):
-        return 20 * x**3 - 79.2 * x**2 +30.72 * x + 42.624
-    
-    def imprimir_nombre(self):
-        return "f2(x)"
+        return self.expresion(x)
 
-class F3(Funcion):
-    
-    def evaluar_funcion(self, x):
-        return (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2)
-    
     def evaluar_derivada_primera(self, x):
-        return (-8 * x + 12) * (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2) + np.exp(-4 * (x - 1.5)**2)
+        return self.derivada_primera(x)
     
     def evaluar_derivada_segunda(self, x):
-        return (-24 * x + (x - 1.5) * (8 *x - 12)**2 + 36) * np.exp(-4 * (x - 1.5)**2)
+        return self.derivada_segunda(x)
     
     def imprimir_nombre(self):
-        return "f3(x)"
+        print(denominacion)
 
+
+f1 = Funcion("f1(x)",
+			lambda x: x**2 - 2,
+			lambda x: 2*x,
+			lambda x: 2
+			)
+f2 = Funcion("f2(x)",
+			lambda x: x**5 - 6.6 * x**4 + 5.12 * x**3 + 21.312 * x**2 - 38.016 * x + 17.28,
+			lambda x: 5 * x**4 -26.4 * x**3 + 15.36 * x**2 + 42.624 * x - 38.016,
+			lambda x: 20 * x**3 - 79.2 * x**2 +30.72 * x + 42.624)
+f3 = Funcion("f3(x)",
+			lambda x: (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2),
+			lambda x: (-8 * x + 12) * (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2) + np.exp(-4 * (x - 1.5)**2),
+			lambda x: (-24 * x + (x - 1.5) * (8 *x - 12)**2 + 36) * np.exp(-4 * (x - 1.5)**2)
+			)
+    
 def imprimir_funcion(funcion):
     nroResolucion = 250
     inicioIntervalo = 0
