5de7b342547135279e4226d63da7f3c79037bd9a
juankristal <juankristal@hotmail.com>
2020-05-27 17:54:37 -0300

    Arreglo identaciones

--- funciones.py
+++ funciones.py
@@ -1,26 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from abc import ABCMeta, abstractmethod
 
 class Funcion():
-    
+	
 	def __init__(self, denominacion, expresion, derivada_primera, derivada_segunda):
 		self.denominacion = denominacion
 		self.expresion = expresion
 		self.derivada_primera = derivada_primera
 		self.derivada_segunda = derivada_segunda
 
-    def evaluar_funcion(self, x):
-        return self.expresion(x)
+	def evaluar_funcion(self, x):
+		return self.expresion(x)
 
-    def evaluar_derivada_primera(self, x):
-        return self.derivada_primera(x)
-    
-    def evaluar_derivada_segunda(self, x):
-        return self.derivada_segunda(x)
-    
-    def imprimir_nombre(self):
-        print(denominacion)
+	def evaluar_derivada_primera(self, x):
+		return self.derivada_primera(x)
+	
+	def evaluar_derivada_segunda(self, x):
+		return self.derivada_segunda(x)
+	
+	def imprimir_nombre(self):
+		print(denominacion)
 
 
 f1 = Funcion("f1(x)",
@@ -37,30 +36,30 @@ f3 = Funcion("f3(x)",
 			lambda x: (-8 * x + 12) * (x - 1.5) * np.exp(-4 * (x - 1.5) ** 2) + np.exp(-4 * (x - 1.5)**2),
 			lambda x: (-24 * x + (x - 1.5) * (8 *x - 12)**2 + 36) * np.exp(-4 * (x - 1.5)**2)
 			)
-    
+	
 def imprimir_funcion(funcion):
-    nroResolucion = 250
-    inicioIntervalo = 0
-    finIntervalo = 2
-    
-    valores = np.zeros((nroResolucion,2))
-    
-    vector_x = np.linspace(inicioIntervalo, finIntervalo, nroResolucion)
-    i = 0
-    for x in vector_x:
-        valores[i] = (x,funcion.evaluar_funcion(x))
-        i = i+1
-    
-    plt.figure()
-    plt.plot(valores[:,0],valores[:,1], '-',lw=2,label=funcion.imprimir_nombre(), color = 'red')    
-    plt.title(funcion.imprimir_nombre())
+	nroResolucion = 250
+	inicioIntervalo = 0
+	finIntervalo = 2
+	
+	valores = np.zeros((nroResolucion,2))
+	
+	vector_x = np.linspace(inicioIntervalo, finIntervalo, nroResolucion)
+	i = 0
+	for x in vector_x:
+		valores[i] = (x,funcion.evaluar_funcion(x))
+		i = i+1
+	
+	plt.figure()
+	plt.plot(valores[:,0],valores[:,1], '-',lw=2,label=funcion.imprimir_nombre(), color = 'red')    
+	plt.title(funcion.imprimir_nombre())
 #    plt.legend(loc='best')
-    plt.grid(True)
-    ax = plt.gca()  # gca stands for 'get current axis'
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    ax.xaxis.set_ticks_position('bottom')
-    ax.spines['bottom'].set_position(('data',0))
-    ax.yaxis.set_ticks_position('left')
-    ax.spines['left'].set_position(('data',0))
-    plt.show()
+	plt.grid(True)
+	ax = plt.gca()  # gca stands for 'get current axis'
+	ax.spines['right'].set_color('none')
+	ax.spines['top'].set_color('none')
+	ax.xaxis.set_ticks_position('bottom')
+	ax.spines['bottom'].set_position(('data',0))
+	ax.yaxis.set_ticks_position('left')
+	ax.spines['left'].set_position(('data',0))
+	plt.show()
